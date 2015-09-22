79ccdeebdca7a169fe58b0871e4b64c429a20d2e
matuzo <manuel@matuzo.at>
2015-09-22 21:24:47 +0200

    update scripts - add script.js - add afix.js - move bootstrap and twitter to cdn

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+$(function() {
+	if($('.localnav').length) {
+		$('h2').each(function() {
+			var id = $(this).attr('id'),
+				text = $(this).text();
+			$a = '<a href="#'+id+'">'+text+'</a>';
+			$li = $('<li class="list-group-item"></li>').append($a);
+			$('.localnav').append($li);
+
+			$('a[href="#'+id+'"]').on('click', function(e) {
+				e.preventDefault();
+
+				$('html, body').animate({
+		        	scrollTop: $("#"+id).offset().top - $('#navbar').height()
+		    	}, 1000);
+
+				history.pushState(null, text, '#'+id);
+			})
+		})
+	}
+
+	console.log(location.href.indexOf('#'))
+
+	if(location.href.indexOf('#') != -1) {
+		var id = location.href.split('#');
+		$('html, body').scrollTop($("#"+id[1]).offset().top - $('#navbar').height());
+	}
+})
--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,15 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify');
 
 gulp.task('mincss', function() {
-  return gulp.src(['assets/style.css', 'bower_components/bootstrap/dist/css/bootstrap.css', 'bower_components/bootstrap/dist/css/bootstrap-theme.css'])
+  return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css', 'bower_components/bootstrap/dist/css/bootstrap-theme.css','assets/style.css'])
     .pipe(concat('style.min.css'))
     .pipe(minifyCss({'keepSpecialComments': 0}))
     .pipe(gulp.dest('assets'));
 });
 
 gulp.task('minjs', function() {
-  return gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap/dist/js/bootstrap.js'])
+  //return gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap/dist/js/bootstrap.js', 'assets/script.js'])
+  return gulp.src(['bower_components/bootstrap/js/affix.js','assets/script.js'])
     .pipe(concat('script.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets'));
@@ -22,4 +23,5 @@ gulp.task('ship', ['mincss', 'minjs']);
 gulp.task('watch', function() {
     // if a less file changes, run the less task
     gulp.watch('assets/style.css', ['mincss']);
+    gulp.watch('assets/script.js', ['minjs']);
 });
